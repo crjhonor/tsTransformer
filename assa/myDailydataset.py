ca7fa2c8f61272aee6651a0afbb49b89737ab023
crjhonor <jaychen175@163.com>
2023-05-04 14:15:06 +0800

    I am trying baidu api to train online and use the model locally by api.

--- assa/myDailydataset.py
+++ assa/myDailydataset.py
@@ -45,6 +45,7 @@ indexList = list(np.unique(indexWanted_CU0 + indexWanted_RB0 + indexWanted_SCM))
 dataDirName = "/home/crjLambda/PycharmProjects/profoundRNN/data"
 
 SEQUENCE_LEN = 25  # stand for 25 trading days which are very close to one month
+SEQUENCE_PAD = 999 # the int used for <pad>
 
 class readingYields:
     def __init__(self, yieldsWanted):
@@ -185,9 +186,9 @@ def generateDatefeature(dataset):
     '''
     returnDataset = dataset.copy()
     # Adding year, month and day
-    returnDataset['day'] = dataset.index.day.values
-    returnDataset['month'] = dataset.index.month.values
     returnDataset['year'] = dataset.index.year.values
+    returnDataset['month'] = dataset.index.month.values
+    returnDataset['day'] = dataset.index.day.values
     # Adding weekdays and businessdays
     weekdays = []
     businessdays = []
@@ -219,6 +220,26 @@ def generate_logr(dataset, isDATE=True):
         returnDataset = dataset_noDATE_logr
     return returnDataset
 
+def myPadding(sequence):
+    lengths = [len(s) for s in sequence]
+    max_lenbth = max(lengths)
+    reSequence = []
+    for s in sequence:
+        if len(s) == max_lenbth:
+            reSequence.append(s)
+        else:
+            num_padding = max_lenbth-len(s)
+            if type(s[0]) == float:
+                sequence_pad =SEQUENCE_PAD
+                for i in range(num_padding):
+                    s.append(sequence_pad)
+            else:
+                sequence_pad = list(np.repeat(SEQUENCE_PAD, len(s[0])))
+                for i in range(num_padding):
+                    s.append(sequence_pad)
+            reSequence.append(s)
+    return reSequence
+
 def generateDataset():
     # Now I join the trading data, yields and sentiment results into one dataset.
     rawDataset = datasetClose.copy()
@@ -228,17 +249,116 @@ def generateDataset():
     rawDataset_logr = generate_logr(rawDataset, isDATE=True)
 
     # Generating features and labels.
+    past_values, future_values, past_time_features, future_time_features = [], [], [], []
     for i in tqdm(range(len(indexesAll_ind)), ncols=100, desc="Generating dataset", colour="blue"):
-        ind = [indexesAll_ind[i]]
 
-    # myDataset = myDataset.join(weiboFeatures_df, rsuffix='_weibo')
-    # myDataset = myDataset.drop(columns='DATE_weibo')
-    # myDataset = myDataset.join(emailFeatures_df, rsuffix='_email')
-    # myDataset = myDataset.drop(columns='DATE_email')
-    # Just drop all NaNs
-    # myDataset = myDataset.dropna()
-    # Generate date feature
-    # myDataset = generateDatefeature(myDataset)
-    # return myDataset
+        # Creating the correlation matrix using rawDataset_logr
+        rawCols =rawDataset_logr.columns.delete([0]) # DATE column is excluded
+        inds = [rawCols[i]]
+        [inds.append(ind) for ind in rawCols[0:i]]
+        [inds.append(ind) for ind in rawCols[i+1:]]
+        beforeCorr = rawDataset_logr[inds]
+        theCorr = beforeCorr.corr() # correlation matrix
+        newOrder = theCorr.iloc[0, :].sort_values(axis=0, ascending=False)
+        afterCorr = beforeCorr[newOrder.index.to_list()]
 
-generateDataset()
+        # Align with sentiment data
+        sentiment_order = afterCorr.shape[1]
+        afterCorr = afterCorr.join(weiboFeatures_df, rsuffix='_weibo')
+        afterCorr = afterCorr.drop(columns='DATE')
+        afterCorr = afterCorr.join(emailFeatures_df, rsuffix='_email')
+        afterCorr = afterCorr.drop(columns='DATE')
+        afterCorr = afterCorr.dropna()
+
+        # Generate Date Time features
+        withDatefeature = generateDatefeature(afterCorr)
+        date_order = afterCorr.shape[1]
+
+        # Generate all the features we need.
+        for l in range(withDatefeature.shape[0]):
+            # past value and past time feature
+            past_value = withDatefeature.iloc[l, :date_order]
+            past_value = past_value.to_list()
+            past_time_feature = withDatefeature.iloc[l, date_order:]
+            past_time_feature = past_time_feature.squeeze(axis=0)
+            past_time_feature = past_time_feature.astype(np.int32)
+            past_time_feature = past_time_feature.to_list()
+            # feature value and future time feature
+            if (l + SEQUENCE_LEN) <= withDatefeature.shape[0]:
+                future_value = withDatefeature.iloc[l:l+SEQUENCE_LEN, 0]
+                future_time_feature = withDatefeature.iloc[l:l+SEQUENCE_LEN, date_order:]
+            else:
+                future_value = withDatefeature.iloc[l:, 0]
+                future_time_feature = withDatefeature.iloc[l:, date_order:]
+            future_value = future_value.to_list()
+            future_time_feature = [future_time_feature.iloc[r, :].to_list() for r in range(future_time_feature.shape[0])]
+            # Append theses features.
+            past_values.append(past_value)
+            past_time_features.append(past_time_feature)
+            future_values.append(future_value)
+            future_time_features.append(future_time_feature)
+
+    print('past values length: {}'.format(len(past_values)))
+    print('past time features length: {}'.format(len(past_time_features)))
+    print('future values length: {}'.format(len(future_values)))
+    print('future time features: {}'.format(len(future_time_features)))
+
+    # One more problem before output to deep learning is that the future values may contain shorter length than SEQUENCE
+    # _LEN than I have to padding them using 999 as the <pad> number.
+    future_values = myPadding(future_values)
+    future_time_features = myPadding(future_time_features)
+
+    # Consider saving all the features into csv file. Before that converting them into DataFrame.
+    returnDataframe = pd.DataFrame({
+        'future_values': future_values,
+        'past_values': past_values,
+        'future_time_features': future_time_features,
+        'past_time_features': past_time_features}
+    )
+    returnDataframe.to_csv('/home/crjLambda/PycharmProjects/profoundRNN/dataForlater/myDatasetfordeeplearning.csv',
+                           index=False)
+
+    # While attempt to use in baidu AI, this is to generate the necessary dataset formate.
+    l_fv = len(future_values[0])
+    baiduDataset_colnames = [''.join(['fv_', str(i)]) for i in range(l_fv)]
+    l_pv = len(past_values[0])
+    for i in range(l_pv):
+        baiduDataset_colnames.append(''.join(['pv_', str(i)]))
+    l_ftf_i = len(future_time_features[0])
+    l_ftf_j = len(future_time_features[0][0])
+    for i in range(l_ftf_i):
+        for j in range(l_ftf_j):
+            baiduDataset_colnames.append(''.join(['ftf_', str(i), '_', str(j)]))
+    l_ptf = len(past_time_features[0])
+    for i in range(l_ptf):
+        baiduDataset_colnames.append(''.join(['ptf_', str(i)]))
+    l_dataset = len(future_values)
+    baiduDataset = pd.DataFrame(index=range(l_dataset), columns=baiduDataset_colnames)
+    for l in tqdm(range(l_dataset), ncols=100, desc="filling baidu dataset", colour="yellow"):
+        for i in range(l_fv):
+            baiduDataset.iloc[l, i] = future_values[l][i]
+        for i in range(l_pv):
+            baiduDataset.iloc[l, i+l_fv] = past_values[l][i]
+        for i in range(l_ftf_i):
+            for j in range(l_ftf_j):
+                baiduDataset.iloc[l, (l_fv+l_pv+i*l_ftf_j+j)] = future_time_features[l][i][j]
+        for i in range(l_ptf):
+            baiduDataset.iloc[l, (l_fv+l_pv+l_ftf_i*l_ftf_j+i)] = past_time_features[l][i]
+
+    # I found biadu AI cannot deal with a dataset of so many columns and it needs a DATE column!!!
+    baiduDataset_slim = baiduDataset.iloc[:, :l_fv+l_pv]
+    baiduDataset_slim['Date'] = [
+        datetime.datetime(year=baiduDataset.loc[i, 'ptf_0'],
+                          month=baiduDataset.loc[i, 'ptf_1'],
+                          day=baiduDataset.loc[i, 'ptf_2']).date() for i in range(l_dataset)
+    ]
+
+    baiduDataset_slim.to_csv(
+        '/home/crjLambda/PycharmProjects/profoundRNN/dataForlater/myDatasetforbaidu.csv',
+        index=False
+    )
+
+    return past_values, past_time_features, future_values, future_time_features
+
+past_values, past_time_features, future_values, future_time_features = generateDataset()
+print('Done')
